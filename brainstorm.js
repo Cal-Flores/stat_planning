943c02bf71cd9d21561b2145d0ec8a0360dd9c50
calebflores <calebflores1388@gmail.com>
2024-10-08 02:57:02 -0500

    kisibe created

---
+++ brainstorm.js
@@ -0,0 +1,97 @@
+STATS:
+
+// Attack      Defense
+// weapon      Shield
+// Magic       Magic Defense
+// Luck        Luck
+// Genjutsu    Willpower
+// Domain      Domain
+// Evasion     Accuracy
+// Ninjutsu    Drain
+// Curse       Negative
+
+[
+    {
+        name: '',
+        nen_type: '0',
+        element: 'Earth',
+        elm: earth,
+        hp: 3000,
+        //PHYSICAL
+        attack: 0,
+        defense: 0,
+        critical: 0.0,
+        strength: 0.0,
+        //MAGIC
+        elm_attack: 0,
+        elm_defense: 0,
+        elm_efficiency: 0.0,
+        potency: 0.0,
+        //WEAPON
+        weapon: 0,
+        shield: 0,
+        //SPEED / LUCK
+        accuracy: 0,
+        evasion: 0,
+        luck: 0,
+        //GENJUTSU
+        genjutsu: 0,
+        gen_power: 0,
+        willpower: 0,
+        chakra: 0,
+        chakra_rec: 0,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 0,
+        cursed_tech: '',
+        curse_amount: 0,
+        curse_duration: 0,
+        curse_cost: 0,
+        //DOMAIN
+        dom_purity: 0,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu: "",
+        nin_cost: 0,
+        ninjutsu_pow: 0,
+        nin2amount: 0,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu2: '',
+        ninJutsu2cost: 0,
+        ninJutsu2Dmg: 0,
+        ninamount: 0,
+        ultEnhance: false,
+        //BUFFS
+        buff: '',
+        buff2: '',
+        buff_acc: 0,
+        buff2_acc: 0,
+        image: 'https://64.media.tumblr.com/bee5b4150e08d22f7351d037813cd8b9/13fcef64824dd1b2-da/s1280x1920/f260dbaffb68a41c3554c15561f909841cf1b9dc.jpg'
+    },
+]
+
+
+
+Attack - 285 | 240 | 200 | 185 | 140
+Defense
+weapon
+Shield
+Magic
+Magic - Defense
+
+Luck - 15 | 10 | 6 | 4 | 2
+Genjutsu
+Willpower
+Purity
+Domain
+Evasion
+Accuracy
+
+Recovery - 12 | 8 | 5 | 4 | 3
+Drain
+
+Curse - 70 | 52 | 35 | 28 | 20
+Negative
---
+++ weaponUsers/nanami.js
@@ -0,0 +1,69 @@
+[
+    {
+        name: 'Nanami',
+        nen_type: 'Enhancer',
+        element: 'Fighter',
+        elm: fighter,
+        hp: 3289,
+        //PHYSICAL
+        attack: 252,
+        defense: 200,
+        critical: .18,
+        strength: 1.5,
+        //MAGIC
+        elm_attack: 127,
+        elm_defense: 186,
+        elm_efficiency: 1,
+        potency: 1,
+        //WEAPON
+        weapon: 274,
+        shield: 236,
+        curse_crit: .85,
+        weaponCurse: 'Shield',
+        weapon_amount: 6,
+        //SPEED / LUCK
+        accuracy: 7,
+        evasion: 3,
+        luck: 7,
+        stun: .08,
+        //GENJUTSU
+        genjutsu: 3,
+        gen_power: 25,
+        willpower: 9,
+        chakra: 0,
+        chakra_rec: 4,
+        drain: 4,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 30,
+        negative_energy: 20,
+        cursed_tech: '',
+        curse_amount: 0,
+        curse_duration: 0,
+        curse_cost: 0,
+        //DOMAIN
+        dom_purity: 2,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu: "",
+        nin_cost: 0,
+        ninjutsu_pow: 0,
+        nin2amount: 0,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu2: '',
+        ninJutsu2cost: 0,
+        ninJutsu2Dmg: 0,
+        ninamount: 0,
+        ultEnhance: false,
+        //BUFFS
+        buff: '',
+        buff2: '',
+        buff_acc: 0,
+        buff2_acc: 0,
+        image: ''
+    },
+
+]
