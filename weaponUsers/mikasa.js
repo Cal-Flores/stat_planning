259bc27134e69645d6bb5a77ee8b6157c600602c
calebflores <calebflores1388@gmail.com>
2024-10-09 16:11:48 -0500

    mikasa added

---
+++ weaponUsers/mikasa.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Mikasa',
+        nen_type: 'Enhancer',
+        element: 'Lightning',
+        elm: lightning,
+        hp: 2971,
+        //PHYSICAL
+        attack: 228,
+        defense: 173,
+        critical: .38,
+        strength: 1.4,
+        //MAGIC
+        elm_attack: 241,
+        elm_defense: 126,
+        elm_efficiency: 1.2,
+        potency: .20,
+        //WEAPON
+        weapon: 245,
+        shield: 169,
+        curse_crit: .20,
+        weaponCurse: 'Defense',
+        weapon_amount: 25,
+        //SPEED / LUCK
+        accuracy: 7,
+        evasion: 8,
+        luck: 6,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 5,
+        gen_power: 15,
+        willpower: 2,
+        chakra: 0,
+        chakra_rec: 2,
+        drain: 4,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 27,
+        negative_energy: 14,
+        cursed_tech: 'Projection-Sorcery',
+        curse_amount: 5,
+        curse_duration: 8,
+        curse_cost: 17,
+        //DOMAIN
+        dom_purity: 5,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Thunder-Spear - Increase Effieciency by 35%',
+        ninJutsu2cost: 13,
+        ninJutsu2Dmg: 245,
+        nin2amount: .35,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Awakening - Increase Critical by 80%",
+        nin_cost: 26,
+        ninjutsu_pow: 0,
+        ninamount: .80,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Shield by 25%',
+        buff2: 'Decrease Accuracy by 2',
+        buff_acc: 70,
+        buff2_acc: 70,
+        image: 'https://preview.redd.it/what-do-you-dislike-about-mikasas-character-v0-s42udm18c6bb1.jpg?auto=webp&s=e7c072a57d924d5aa9bd83dbae93ed7bc93c160f'
+    },
+]
