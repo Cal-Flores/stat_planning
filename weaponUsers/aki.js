3504cda8645ef760cd4df06359ca2299c48201f2
calebflores <calebflores1388@gmail.com>
2024-10-10 20:49:35 -0500

    added aki

---
+++ weaponUsers/aki.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Aki',
+        nen_type: 'Specialist',
+        element: 'Dark',
+        elm: dark,
+        hp: 3077,
+        //PHYSICAL
+        attack: 133,
+        defense: 200,
+        critical: .70,
+        strength: 1.7,
+        //MAGIC
+        elm_attack: 135,
+        elm_defense: 170,
+        elm_efficiency: 1.2,
+        potency: .10,
+        //WEAPON
+        weapon: 272,
+        shield: 198,
+        curse_crit: .70,
+        weaponCurse: 'Shield',
+        weapon_amount: 7,
+        //SPEED / LUCK
+        accuracy: 7,
+        evasion: 7,
+        luck: 7,
+        stun: .07,
+        //GENJUTSU
+        genjutsu: 3,
+        gen_power: 15,
+        willpower: 2,
+        chakra: 0,
+        chakra_rec: 7,
+        drain: 2,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 28,
+        negative_energy: 34,
+        cursed_tech: 'Blade of the right hand - Add 50 to attack',
+        curse_amount: 50,
+        curse_duration: 6,
+        curse_cost: 17,
+        //DOMAIN
+        dom_purity: 9,
+        dom_lethal: 40,
+        dom_duration: 8,
+        dom_cost: 27,
+        //NINJUTSU
+        ninjutsu2: 'Future-Devil Contract - Increase Evasion by 1',
+        ninJutsu2cost: 17,
+        ninJutsu2Dmg: 177,
+        nin2amount: 1,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Curse Devil Contract I - Halve Any stat",
+        nin_cost: 45,
+        ninjutsu_pow: 0,
+        ninamount: 0,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Curse Devil Contract II',
+        buff2: 'Curse Devil Contract III - Erase 1 stat',
+        buff_acc: 77,
+        buff2_acc: 57,
+        image: 'https://i.pinimg.com/736x/a8/26/45/a826458e9ec3b3e63687e831a0841546.jpg'
+    },
+]
