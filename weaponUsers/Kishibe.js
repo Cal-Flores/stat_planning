32b1956f9f087f1a29ce48fabba25932d093313d
calebflores <calebflores1388@gmail.com>
2024-10-10 21:42:43 -0500

    added kisibe

---
+++ weaponUsers/Kishibe.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Kishibe',
+        nen_type: 'Enhancer',
+        element: 'fighter',
+        elm: fighter,
+        hp: 3333,
+        //PHYSICAL
+        attack: 240,
+        defense: 233,
+        critical: .15,
+        strength: 2,
+        //MAGIC
+        elm_attack: 155,
+        elm_defense: 168,
+        elm_efficiency: 1.2,
+        potency: .10,
+        //WEAPON
+        weapon: 255,
+        shield: 234,
+        curse_crit: 0.12,
+        weaponCurse: 'Accuracy',
+        weapon_amount: 1,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 7,
+        luck: 2,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 3,
+        gen_power: 20,
+        willpower: 5,
+        chakra: 0,
+        chakra_rec: 3,
+        drain: 3,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 34,
+        negative_energy: 35,
+        cursed_tech: 'Projection-Sorcery - Double Evasion',
+        curse_amount: 7,
+        curse_duration: 6,
+        curse_cost: 19,
+        //DOMAIN
+        dom_purity: 2,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Knife Devil Contract - Increase Weapon by 8%',
+        ninJutsu2cost: 15,
+        ninJutsu2Dmg: 185,
+        nin2amount: .08,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Claw Devil contract - Increase Attack by 8%",
+        nin_cost: 15,
+        ninjutsu_pow: 185,
+        ninamount: .08,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Double Wither percent',
+        buff2: 'Decrease Potency by 50%',
+        buff_acc: 60,
+        buff2_acc: 65,
+        image: 'https://preview.redd.it/i-tried-to-make-kishibe-a-bit-more-lively-v0-ke8dx7n9oxoa1.jpg?width=640&crop=smart&auto=webp&s=30f8a2bebb02a1782f1418be2c76debf6493d0e9'
+    },
+]
