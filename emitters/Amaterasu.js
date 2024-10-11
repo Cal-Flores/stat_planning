2d62e25416fc8d9d7562d820c26d7b52e64ca7b5
calebflores <calebflores1388@gmail.com>
2024-10-11 14:37:45 -0500

    Amaterasu Sasuke  added

---
+++ emitters/Amaterasu.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Amaterasu Sasuke',
+        nen_type: 'Emitter',
+        element: 'Fire',
+        elm: fire,
+        hp: 3264,
+        //PHYSICAL
+        attack: 158,
+        defense: 240,
+        critical: .15,
+        strength: 1.3,
+        //MAGIC
+        elm_attack: 280,
+        elm_defense: 185,
+        elm_efficiency: 1.50,
+        potency: .20,
+        //WEAPON
+        weapon: 166,
+        shield: 225,
+        curse_crit: .10,
+        weaponCurse: 'Weapon',
+        weapon_amount: 5,
+        //SPEED / LUCK
+        accuracy: 7,
+        evasion: 2,
+        luck: 2,
+        stun: .04,
+        //GENJUTSU
+        genjutsu: 5,
+        gen_power: 35,
+        willpower: 7,
+        chakra: 0,
+        chakra_rec: 4,
+        drain: 2,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 15,
+        negative_energy: 15,
+        cursed_tech: 'Disaster-Flames - Increase Potency to .50',
+        curse_amount: .30,
+        curse_duration: 6,
+        curse_cost: 12,
+        //DOMAIN
+        dom_purity: 7,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Flame-Control Amaterasu - Increase Magic Defense by 10%',
+        ninJutsu2cost: 15,
+        ninJutsu2Dmg: 175,
+        nin2amount: 10,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Amaterasu-Arrow - Decrease Elemental defense by 25%",
+        nin_cost: 28,
+        ninjutsu_pow: 250,
+        ninamount: .25,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Weapon by 20%',
+        buff2: 'Decrease Evasion by 2',
+        buff_acc: 65,
+        buff2_acc: 65,
+        image: 'https://w0.peakpx.com/wallpaper/513/74/HD-wallpaper-sasuke-black-and-white-sharingan-sasuke-black-uchiha-amaterasu-white-mangekyou-thumbnail.jpg'
+    },
+]
