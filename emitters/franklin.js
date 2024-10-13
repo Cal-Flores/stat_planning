c256dba57f10ba086e3e53dc978bddc45ccbf331
calebflores <calebflores1388@gmail.com>
2024-10-12 23:28:49 -0500

    franklin added

---
+++ emitters/franklin.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Franklin',
+        nen_type: 'Emitter',
+        element: 'Earth',
+        elm: earth,
+        hp: 3318,
+        //PHYSICAL
+        attack: 128,
+        defense: 265,
+        critical: .05,
+        strength: 1,
+        //MAGIC
+        elm_attack: 247,
+        elm_defense: 252,
+        elm_efficiency: 1.1,
+        potency: .25,
+        //WEAPON
+        weapon: 214,
+        shield: 164,
+        curse_crit: .12,
+        weaponCurse: 'Magic-Def',
+        weapon_amount: 15,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 2,
+        luck: 5,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 2,
+        chakra: 0,
+        chakra_rec: 3,
+        drain: 10,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 22,
+        negative_energy: 20,
+        cursed_tech: 'Granite-Blast Deal 250 Damage',
+        curse_amount: 250,
+        curse_duration: 1,
+        curse_cost: 20,
+        //DOMAIN
+        dom_purity: 3,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Nen-Bullets - Lower Magic Defense by 10%',
+        ninJutsu2cost: 14,
+        ninJutsu2Dmg: 175,
+        nin2amount: .10,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Double-Machine-Gun - Increase Potency by 25%",
+        nin_cost: 18,
+        ninjutsu_pow: 235,
+        ninamount: .25,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Weapon by 30%',
+        buff2: 'Decrease Shield by 30%',
+        buff_acc: 70,
+        buff2_acc: 65,
+        image: 'https://static.wikia.nocookie.net/hunterxhunter/images/6/62/HxH2011_EP58_Franklin_Portrait.png/revision/latest?cb=20230122063251'
+    },
+]
