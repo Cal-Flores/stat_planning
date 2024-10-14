92744682df5f60be8c183d0c735aa4e2f07269dc
Amy Jones <amymjones55@gmail.com>
2024-10-13 21:30:20 -0500

    temari added

---
+++ emitters/temari.js
@@ -0,0 +1,67 @@
+let temari =
+{
+        name: 'Temari',
+        nen_type: '',
+        element: 'Wind',
+        elm: wind,
+        hp: 2962,
+        //PHYSICAL
+        attack: 134,
+        defense: 161,
+        critical: .28,
+        strength: 1.4,
+        //MAGIC
+        elm_attack: 246,
+        elm_defense: 193,
+        elm_efficiency: 1.65,
+        potency: 0.45,
+        //WEAPON
+        weapon: 230,
+        shield: 168,
+        curse_crit: .40,
+        weaponCurse: 'Magic',
+        weapon_amount: 9,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 3,
+        luck: 6,
+        stun: .06,
+        //GENJUTSU
+        genjutsu: 3,
+        gen_power: 10,
+        willpower: 4,
+        chakra: 0,
+        chakra_rec: 4,
+        drain: 5,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 32,
+        negative_energy: 28,
+        cursed_tech: 'Construction-Technique - Increase Defense by 100',
+        curse_amount: 100,
+        curse_duration: 6,
+        curse_cost: 16,
+        //DOMAIN
+        dom_purity: 4,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Kamaitachi - Increase Weapon Critical by 20%',
+        ninJutsu2cost: 18,
+        ninJutsu2Dmg: 165,
+        nin2amount: .20,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: 'Kamikaze  - Traps Enemy in Wind-Vortex for 1 turn',
+        nin_cost: 24,
+        ninjutsu_pow: 175,
+        ninamount: 0,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Magic Attack by 8%',
+        buff2: 'Decrease Attack by 20%',
+        buff_acc: 65,
+        buff2_acc: 70,
+        image: 'https://static.wikia.nocookie.net/naruto/images/b/b2/Temari_Part_II.png/revision/latest/scale-to-width-down/300?cb=20180131102417'
+    }
