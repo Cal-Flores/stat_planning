8d57886fef30b39bab080c4c77666e0ec86fea22
calebflores <calebflores1388@gmail.com>
2024-10-13 21:30:17 -0500

    yamato added

---
+++ emitters/pokkle.js
@@ -0,0 +1,69 @@
+[
+    {
+        name: 'Pokkle',
+        nen_type: 'Emitter',
+        element: 'Fairy',
+        elm: fairy,
+        hp: 2893,
+        //PHYSICAL
+        attack: 118,
+        defense: 156,
+        critical: .10,
+        strength: 1.3,
+        //MAGIC
+        elm_attack: 239,
+        elm_defense: 238,
+        elm_efficiency: 1.5,
+        potency: .35,
+        //WEAPON
+        weapon: 226,
+        shield: 172,
+        curse_crit: .45,
+        weaponCurse: 'Curse',
+        weapon_amount: 8,
+        //SPEED / LUCK
+        accuracy: 8,
+        evasion: 5,
+        luck: 7,
+        stun: .08,
+        //GENJUTSU
+        genjutsu: 6,
+        gen_power: 60,
+        willpower: 2,
+        chakra: 0,
+        chakra_rec: 6,
+        drain: 2,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 25,
+        negative_energy: 15,
+        cursed_tech: 'Seven-Spectrum - Minus 6 Cost Ninjutsu',
+        curse_amount: 6,
+        curse_duration: 6,
+        curse_cost: 15,
+        //DOMAIN
+        dom_purity: 1,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Red-Arrow - Increase Magic by 5%',
+        ninJutsu2cost: 13,
+        ninJutsu2Dmg: 135,
+        nin2amount: .05,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Orange-Arrow - Increase Evasion by 1",
+        nin_cost: 18,
+        ninjutsu_pow: 145,
+        ninamount: 1,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Double Stun rate',
+        buff2: 'Decrease Accuracy by 2',
+        buff_acc: 70,
+        buff2_acc: 60,
+        image: ''
+    },
+
+]
