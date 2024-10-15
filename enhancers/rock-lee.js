da3d5b973119bddb08d5afade39734274e3773be
calebflores <calebflores1388@gmail.com>
2024-10-15 14:23:02 -0500

    rock lee  added

---
+++ enhancers/rock-lee.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Rock Lee',
+        nen_type: 'Engancer',
+        element: 'Earth',
+        elm: earth,
+        hp: 2872,
+        //PHYSICAL
+        attack: 234,
+        defense: 194,
+        critical: .24,
+        strength: 1.7,
+        //MAGIC
+        elm_attack: 106,
+        elm_defense: 152,
+        elm_efficiency: 1,
+        potency: .08,
+        //WEAPON
+        weapon: 211,
+        shield: 232,
+        curse_crit: .08,
+        weaponCurse: 'Recovery',
+        weapon_amount: 1,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 8,
+        luck: 7,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 1,
+        gen_power: 0,
+        willpower: 2,
+        chakra: 0,
+        chakra_rec: 4,
+        drain: 4,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 45,
+        negative_energy: 22,
+        cursed_tech: '8th-Gate - Increase Attack by 100',
+        curse_amount: 100,
+        curse_duration: 100,
+        curse_cost: 45,
+        //DOMAIN
+        dom_purity: 2,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Leaf-Hurricane - Increase stun by 6%',
+        ninJutsu2cost: 14,
+        ninJutsu2Dmg: 200,
+        nin2amount: .06,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Gate-Release - Increase Evasion by 1",
+        nin_cost: 20,
+        ninjutsu_pow: 0,
+        ninamount: 1,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Curse Recovery by 15',
+        buff2: 'Decrease Defense by 10%',
+        buff_acc: 50,
+        buff2_acc: 70,
+        image: 'https://imgix.ranker.com/user_node_img/50088/1001756931/original/the-lotus-of-the-leaf-village-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=355'
+    },
+]
