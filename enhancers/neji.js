b565e740332417e57a8436dfdc9d8dfa2e304c0f
calebflores <calebflores1388@gmail.com>
2024-10-15 05:06:09 -0500

    neji  added

---
+++ enhancers/neji.js
@@ -0,0 +1,67 @@
+[{
+    name: 'Neji',
+    nen_type: 'Enhancer',
+    element: 'Fairy',
+    elm: fairy,
+    hp: 2875,
+    //PHYSICAL
+    attack: 235,
+    defense: 174,
+    critical: .65,
+    strength: 1.2,
+    //MAGIC
+    elm_attack: 165,
+    elm_defense: 220,
+    elm_efficiency: 1.4,
+    potency: .15,
+    //WEAPON
+    weapon: 148,
+    shield: 190,
+    curse_crit: .06,
+    weaponCurse: 'Purity',
+    weapon_amount: 1,
+    //SPEED / LUCK
+    accuracy: 9,
+    evasion: 3,
+    luck: 2,
+    stun: .06,
+    //GENJUTSU
+    genjutsu: 1,
+    gen_power: 10,
+    willpower: 5,
+    chakra: 0,
+    chakra_rec: 5,
+    drain: 2,
+    //CURSE
+    cursed_energy: 0,
+    cursed_recovery: 50,
+    negative_energy: 12,
+    cursed_tech: 'Domain- Amplification Increase Domain Duration by 4',
+    curse_amount: 4,
+    curse_duration: 100,
+    curse_cost: 18,
+    //DOMAIN
+    dom_purity: 6,
+    dom_lethal: 85,
+    dom_duration: 8,
+    dom_cost: 26,
+    //NINJUTSU
+    ninjutsu2: '8-Trigram-64-Palms - Increase Stun by 9%',
+    ninJutsu2cost: 12,
+    ninJutsu2Dmg: 100,
+    nin2amount: .09,
+    ninEnhance: true,
+    //ULTIMATE
+    ninjutsu: "Rotation - Increase Sheild by 15%",
+    nin_cost: 16,
+    ninjutsu_pow: 200,
+    ninamount: .15,
+    ultEnhance: true,
+    //BUFFS
+    buff: 'Increase Accuracy by 5',
+    buff2: 'Decrease Stun by 100%',
+    buff_acc: 50,
+    buff2_acc: 65,
+    image: 'https://static.wikia.nocookie.net/eroninja/images/7/79/Neji.png/revision/latest?cb=20180124002919'
+},
+]
