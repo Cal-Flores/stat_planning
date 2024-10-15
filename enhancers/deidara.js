af5a7d1fd7d85cf717d697e57820964bb476773b
calebflores <calebflores1388@gmail.com>
2024-10-15 04:05:22 -0500

    deidara added

---
+++ enhancers/deidara.js
@@ -0,0 +1,69 @@
+[
+    {
+        name: 'Deidara',
+        nen_type: 'Conjuror',
+        element: 'Earth',
+        elm: earth,
+        hp: 2992,
+        //PHYSICAL
+        attack: 238,
+        defense: 188,
+        critical: .60,
+        strength: 1.35,
+        //MAGIC
+        elm_attack: 218,
+        elm_defense: 196,
+        elm_efficiency: 1.5,
+        potency: .20,
+        //WEAPON
+        weapon: 110,
+        shield: 225,
+        curse_crit: .08,
+        weaponCurse: 'Gen-Power',
+        weapon_amount: 15,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 5,
+        luck: 2,
+        stun: .07,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 5,
+        chakra: 0,
+        chakra_rec: 7,
+        drain: 4,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 15,
+        negative_energy: 14,
+        cursed_tech: 'Granite-Blast',
+        curse_amount: 300,
+        curse_duration: 1,
+        curse_cost: 18,
+        //DOMAIN
+        dom_purity: 6,
+        dom_lethal: 50,
+        dom_duration: 6,
+        dom_cost: 30,
+        //NINJUTSU
+        ninjutsu2: 'C3 - Increase Attack by 8%',
+        ninJutsu2cost: 12,
+        ninJutsu2Dmg: 145,
+        nin2amount: .08,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "C4 Karura - Increase Lethal by 20",
+        nin_cost: 24,
+        ninjutsu_pow: 0,
+        ninamount: 20,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Evasion by 3',
+        buff2: 'Decrease Purity by 3',
+        buff_acc: 60,
+        buff2_acc: 60,
+        image: 'https://static.wikia.nocookie.net/naruto/images/3/33/Deidara_Eye_Scope.png/revision/latest?cb=20160109162348'
+    },
+
+]
