e02015cd4f923ad3272f927077a1921e23c03cb2
calebflores <calebflores1388@gmail.com>
2024-10-15 03:48:28 -0500

    yuji added

---
+++ enhancers/itadori.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Itadori',
+        nen_type: 'Enhancer',
+        element: 'Ghost',
+        elm: ghost,
+        hp: 3396,
+        //PHYSICAL
+        attack: 261,
+        defense: 246,
+        critical: .30,
+        strength: 1.65,
+        //MAGIC
+        elm_attack: 134,
+        elm_defense: 197,
+        elm_efficiency: 1.4,
+        potency: .40,
+        //WEAPON
+        weapon: 171,
+        shield: 155,
+        curse_crit: .20,
+        weaponCurse: 'Critical',
+        weapon_amount: .10,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 6,
+        luck: 7,
+        stun: .07,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 40,
+        willpower: 4,
+        chakra: 0,
+        chakra_rec: 3,
+        drain: 3,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 66,
+        negative_energy: 35,
+        cursed_tech: 'Black-Flash - 10/20',
+        curse_amount: 0,
+        curse_duration: 2,
+        curse_cost: 10,
+        //DOMAIN
+        dom_purity: 6,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Increase Defense by 8%',
+        ninJutsu2cost: 12,
+        ninJutsu2Dmg: 150,
+        nin2amount: .08,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Increase Shield by 10%",
+        nin_cost: 15,
+        ninjutsu_pow: 155,
+        ninamount: .10,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Curse Recvoery by 15',
+        buff2: 'Decrease Curse Recovery by 15',
+        buff_acc: 65,
+        buff2_acc: 65,
+        image: 'https://i.pinimg.com/736x/10/80/8d/10808dc6cd9b78f0153e68aae9942abc.jpg'
+    },
+]
