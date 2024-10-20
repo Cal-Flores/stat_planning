f8f5a83a41a862c93e7fbbe6e84f4855364aa751
calebflores <calebflores1388@gmail.com>
2024-10-20 18:20:18 -0500

    anbu itachi added

---
+++ genjutsu/momo.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Momo',
+        nen_type: 'Specialist',
+        element: 'Wind',
+        elm: wind,
+        hp: 2762,
+        //PHYSICAL
+        attack: 110,
+        defense: 131,
+        critical: .06,
+        strength: 1.05,
+        //MAGIC
+        elm_attack: 229,
+        elm_defense: 238,
+        elm_efficiency: 1.75,
+        potency: .20,
+        //WEAPON
+        weapon: 135,
+        shield: 138,
+        curse_crit: .10,
+        weaponCurse: 'Accuracy',
+        weapon_amount: 1,
+        //SPEED / LUCK
+        accuracy: 9,
+        evasion: 5,
+        luck: 6,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 8,
+        gen_power: 56,
+        willpower: 0,
+        chakra: 0,
+        chakra_rec: 8,
+        drain: 8,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 30,
+        negative_energy: 25,
+        cursed_tech: 'Albatross - Increase Magic by 60',
+        curse_amount: 60,
+        curse_duration: 6,
+        curse_cost: 18,
+        //DOMAIN
+        dom_purity: 2,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Wind-Scythe - Increase Magic by 5%',
+        ninJutsu2cost: 14,
+        ninJutsu2Dmg: 180,
+        nin2amount: .05,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Cursed-Broom - Increase Genjutsu Power by 10",
+        nin_cost: 18,
+        ninjutsu_pow: 0,
+        ninamount: 10,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Evasion by 3',
+        buff2: 'Decrease Magic Defense by 20%',
+        buff_acc: 60,
+        buff2_acc: 50,
+        image: 'https://image.tensorartassets.com/model_showcase/645968480362936778/252404c0-4a75-a16b-7f79-d30aca612a47.jpeg'
+    },
+]
