1f92e71ac4aee31c32d0104d4644731c72a99400
calebflores <calebflores1388@gmail.com>
2024-10-20 01:26:41 -0500

    anbu itachi added

---
+++ enhancers/anbu_itachi.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Anbu Itachi',
+        nen_type: 'Manipulator',
+        element: 'Fire',
+        elm: fire,
+        hp: 3132,
+        //PHYSICAL
+        attack: 154,
+        defense: 167,
+        critical: .18,
+        strength: 1.3,
+        //MAGIC
+        elm_attack: 180,
+        elm_defense: 175,
+        elm_efficiency: 1.2,
+        potency: .40,
+        //WEAPON
+        weapon: 221,
+        shield: 207,
+        curse_crit: .15,
+        weaponCurse: 'Willpower',
+        weapon_amount: 2,
+        //SPEED / LUCK
+        accuracy: 9,
+        evasion: 7,
+        luck: 4,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 10,
+        gen_power: 100,
+        willpower: 7,
+        chakra: 0,
+        chakra_rec: 12,
+        drain: 6,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 15,
+        negative_energy: 30,
+        cursed_tech: 'Black-Bird-Manipulation - Increase Genjutsu Power by 50',
+        curse_amount: 50,
+        curse_duration: 6,
+        curse_cost: 20,
+        //DOMAIN
+        dom_purity: 9,
+        dom_lethal: 150,
+        dom_duration: 4,
+        dom_cost: 25,
+        //NINJUTSU
+        ninjutsu2: 'Totsuka-Blade - Increase Weapon by 15%',
+        ninJutsu2cost: 18,
+        ninJutsu2Dmg: 190,
+        nin2amount: .15,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Tsukoyomi - Increase Duration by 1",
+        nin_cost: 26,
+        ninjutsu_pow: 0,
+        ninamount: 1,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Defense by 20%',
+        buff2: 'Decrease Weapon by 15%',
+        buff_acc: 70,
+        buff2_acc: 70,
+        image: 'https://fbi.cults3d.com/uploaders/18749279/illustration-file/1861899f-985b-4d5c-b20c-7963bab4409a/tumblr_40988e0d72ec3b0ef605d977222b3828_e2a7cb11_1280.jpg'
+    },
+]
