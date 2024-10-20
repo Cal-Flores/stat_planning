f5c302d32933ba8378d4f1f2e62827fe2a8f1bd3
calebflores <calebflores1388@gmail.com>
2024-10-20 02:43:17 -0500

    anbu itachi added

---
+++ genjutsu/illumi.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Illumi',
+        nen_type: 'Manipulator',
+        element: 'Dark',
+        elm: dark,
+        hp: 3162,
+        //PHYSICAL
+        attack: 129,
+        defense: 137,
+        critical: .20,
+        strength: 1.1,
+        //MAGIC
+        elm_attack: 153,
+        elm_defense: 234,
+        elm_efficiency: 1.9,
+        potency: .12,
+        //WEAPON
+        weapon: 212,
+        shield: 168,
+        curse_crit: .35,
+        weaponCurse: 'Willpower',
+        weapon_amount: 1,
+        //SPEED / LUCK
+        accuracy: 9,
+        evasion: 6,
+        luck: 3,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 10,
+        gen_power: 78,
+        willpower: 6,
+        chakra: 0,
+        chakra_rec: 12,
+        drain: 4,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 55,
+        negative_energy: 30,
+        cursed_tech: 'Brain-Manipulation - Decrease any stat by 17%',
+        curse_amount: 0,
+        curse_duration: 1,
+        curse_cost: 16,
+        //DOMAIN
+        dom_purity: 5,
+        dom_lethal: 35,
+        dom_duration: 9,
+        dom_cost: 27,
+        //NINJUTSU
+        ninjutsu2: 'Body-Alteration - Increase Weapon by 8%',
+        ninJutsu2cost: 14,
+        ninJutsu2Dmg: 125,
+        nin2amount: .08,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Hypnotic-Spell - Increase Lethal by 15",
+        nin_cost: 19,
+        ninjutsu_pow: 0,
+        ninamount: 15,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Purity by 2',
+        buff2: 'Decrease Critical by 50%',
+        buff_acc: 65,
+        buff2_acc: 60,
+        image: 'https://static.wikia.nocookie.net/hunterxhunter/images/7/72/Illumi_Zoldyck_HCE_Portrait.png/revision/latest/scale-to-width/360?cb=20181227022223'
+    },
+]
---
+++ genjutsu/shalnark.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Shalnark',
+        nen_type: 'Manipulator',
+        element: 'Grass',
+        elm: grass,
+        hp: 2928,
+        //PHYSICAL
+        attack: 183,
+        defense: 198,
+        critical: .15,
+        strength: 2,
+        //MAGIC
+        elm_attack: 185,
+        elm_defense: 166,
+        elm_efficiency: 1.4,
+        potency: .20,
+        //WEAPON
+        weapon: 124,
+        shield: 229,
+        curse_crit: .18,
+        weaponCurse: 'Willpower',
+        weapon_amount: 2,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 4,
+        luck: 7,
+        stun: .04,
+        //GENJUTSU
+        genjutsu: 9,
+        gen_power: 68,
+        willpower: 9,
+        chakra: 0,
+        chakra_rec: 8,
+        drain: 8,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 45,
+        negative_energy: 24,
+        cursed_tech: 'Brain-Manipulation - Decrease Any Stat by 15%',
+        curse_amount: 0,
+        curse_duration: 1,
+        curse_cost: 16,
+        //DOMAIN
+        dom_purity: 2,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Black-Voice-Control - Decrease Willpower by 1',
+        ninJutsu2cost: 15,
+        ninJutsu2Dmg: 0,
+        nin2amount: 1,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Auto-Pilot-Mode - Increase Critical to 70%",
+        nin_cost: 60,
+        ninjutsu_pow: 500,
+        ninamount: .55,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Double Attack',
+        buff2: 'Decrease Accuracy by 2',
+        buff_acc: 45,
+        buff2_acc: 50,
+        image: ''
+    },
+]
