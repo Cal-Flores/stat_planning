234eb6f886359f49a3d7afcc4ec5a00f2bc32a66
calebflores <calebflores1388@gmail.com>
2024-10-20 16:27:02 -0500

    anbu itachi added

---
+++ genjutsu/ponzu.js
@@ -0,0 +1,69 @@
+[
+    {
+        name: 'Ponzu',
+        nen_type: 'Manipulator',
+        element: 'Fairy',
+        elm: fairy,
+        hp: 2790,
+        //PHYSICAL
+        attack: 113,
+        defense: 125,
+        critical: .08,
+        strength: 1.1,
+        //MAGIC
+        elm_attack: 213,
+        elm_defense: 208,
+        elm_efficiency: 1.6,
+        potency: .30,
+        //WEAPON
+        weapon: 140,
+        shield: 165,
+        curse_crit: .10,
+        weaponCurse: 'Evasion',
+        weapon_amount: 1,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 5,
+        luck: 8,
+        stun: .35,
+        //GENJUTSU
+        genjutsu: 7,
+        gen_power: 60,
+        willpower: 5,
+        chakra: 0,
+        chakra_rec: 8,
+        drain: 7,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 32,
+        negative_energy: 28,
+        cursed_tech: 'Miracle - Increase Luck by 5',
+        curse_amount: 5,
+        curse_duration: 4,
+        curse_cost: 20,
+        //DOMAIN
+        dom_purity: 3,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Sting - Increase Stun by 10%',
+        ninJutsu2cost: 14,
+        ninJutsu2Dmg: 100,
+        nin2amount: .10,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Bee-Manipulation - Paralyzed for 1 turn",
+        nin_cost: 18,
+        ninjutsu_pow: 170,
+        ninamount: 0,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Shield by 25%',
+        buff2: 'Decrease Accuracy by 2',
+        buff_acc: 70,
+        buff2_acc: 50,
+        image: ''
+    },
+
+]
