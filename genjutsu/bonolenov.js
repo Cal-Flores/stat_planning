37252f8b1f4d76033265aa6dbbfb5f95475b62ad
calebflores <calebflores1388@gmail.com>
2024-10-20 15:04:17 -0500

    anbu itachi added

---
+++ genjutsu/bonolenov.js
@@ -0,0 +1,69 @@
+[
+    {
+        name: 'Bonolenov',
+        nen_type: 'Conjuror',
+        element: 'Wind',
+        elm: wind,
+        hp: 3009,
+        //PHYSICAL
+        attack: 184,
+        defense: 165,
+        critical: .20,
+        strength: 1.4,
+        //MAGIC
+        elm_attack: 224,
+        elm_defense: 229,
+        elm_efficiency: 2,
+        potency: .25,
+        //WEAPON
+        weapon: 130,
+        shield: 168,
+        curse_crit: .06,
+        weaponCurse: 'Accuracy',
+        weapon_amount: 2,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 3,
+        luck: 7,
+        stun: .06,
+        //GENJUTSU
+        genjutsu: 9,
+        gen_power: 70,
+        willpower: 4,
+        chakra: 0,
+        chakra_rec: 10,
+        drain: 5,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 45,
+        negative_energy: 35,
+        cursed_tech: 'Simple-Barrier - Increase Magic Defense by 50',
+        curse_amount: 50,
+        curse_duration: 6,
+        curse_cost: 12,
+        //DOMAIN
+        dom_purity: 7,
+        dom_lethal: 80,
+        dom_duration: 6,
+        dom_cost: 27,
+        //NINJUTSU
+        ninjutsu2: 'Battle-Cantabile-Jupiter - Increase Magic 5%',
+        ninJutsu2cost: 14,
+        ninJutsu2Dmg: 185,
+        nin2amount: .05,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Battle-Cantabile-Metamorphosen - Increase Lethal by 25",
+        nin_cost: 34,
+        ninjutsu_pow: 165,
+        ninamount: 25,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Magic by 25%',
+        buff2: 'Decrease Accuracy by 3',
+        buff_acc: 60,
+        buff2_acc: 40,
+        image: 'https://static.wikia.nocookie.net/hunterxhunter/images/f/fc/HxH2011_EP96_Bonolenov_Portrait.png/revision/latest?cb=20230123153232'
+    },
+
+]
---
+++ genjutsu/shino.js
@@ -0,0 +1,69 @@
+[
+    {
+        name: 'Shino',
+        nen_type: 'Manipulator',
+        element: 'Grass',
+        elm: grass,
+        hp: 2852,
+        //PHYSICAL
+        attack: 205,
+        defense: 204,
+        critical: .16,
+        strength: 1.4,
+        //MAGIC
+        elm_attack: 113,
+        elm_defense: 132,
+        elm_efficiency: 1.1,
+        potency: .12,
+        //WEAPON
+        weapon: 169,
+        shield: 187,
+        curse_crit: .12,
+        weaponCurse: 'Recovery',
+        weapon_amount: 1,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 4,
+        luck: 6,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 9,
+        gen_power: 62,
+        willpower: 8,
+        chakra: 0,
+        chakra_rec: 9,
+        drain: 9,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 45,
+        negative_energy: 50,
+        cursed_tech: 'Harden - Increase Defense by 70',
+        curse_amount: 70,
+        curse_duration: 6,
+        curse_cost: 18,
+        //DOMAIN
+        dom_purity: 5,
+        dom_lethal: 100,
+        dom_duration: 4,
+        dom_cost: 28,
+        //NINJUTSU
+        ninjutsu2: 'Insect-Sphere - Increase Ninjutsu Power by 8%',
+        ninJutsu2cost: 20,
+        ninJutsu2Dmg: 240,
+        nin2amount: .08,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Parasitic-Destruction - Decrease Recovery by 2",
+        nin_cost: 24,
+        ninjutsu_pow: 190,
+        ninamount: 2,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Luck by 2',
+        buff2: 'Decrease Strength by 70%',
+        buff_acc: 65,
+        buff2_acc: 65,
+        image: 'https://static.wikia.nocookie.net/naruto/images/e/e1/Shino_Aburame_P2.png/revision/latest/scale-to-width-down/300?cb=20160927065621'
+    },
+
+]
