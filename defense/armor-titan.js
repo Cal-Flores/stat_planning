ef97c0212b7dc521c41f67cf7aeb98d690cfe51b
calebflores <calebflores1388@gmail.com>
2024-10-20 22:38:50 -0500

    defense types added

---
+++ defense/armor-titan.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Armor Titan',
+        nen_type: 'Transmuter',
+        element: 'Earth',
+        elm: earth,
+        hp: 3500,
+        //PHYSICAL
+        attack: 225,
+        defense: 255,
+        critical: .15,
+        strength: 1.65,
+        //MAGIC
+        elm_attack: 125,
+        elm_defense: 230,
+        elm_efficiency: 1,
+        potency: .05,
+        //WEAPON
+        weapon: 166,
+        shield: 285,
+        curse_crit: .06,
+        weaponCurse: 'Evasion',
+        weapon_amount: 1,
+        //SPEED / LUCK
+        accuracy: 4,
+        evasion: 3,
+        luck: 5,
+        stun: .04,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 3,
+        chakra: 0,
+        chakra_rec: 3,
+        drain: 2,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 25,
+        negative_energy: 15,
+        cursed_tech: 'Muscular-Expansion - Critical x 4',
+        curse_amount: .45,
+        curse_duration: 6,
+        curse_cost: 16,
+        //DOMAIN
+        dom_purity: 6,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Harden - Increase Accuracy by 1',
+        ninJutsu2cost: 9,
+        ninJutsu2Dmg: 0,
+        nin2amount: 1,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "RAM - Decrease Defense by 10%",
+        nin_cost: 18,
+        ninjutsu_pow: 260,
+        ninamount: .10,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Defense by 10%',
+        buff2: 'Decrease Domain Purity by 2',
+        buff_acc: 70,
+        buff2_acc: 50,
+        image: 'https://i.redd.it/k2fme3l6e8m61.jpg'
+    },
+]
