38cff74d941c0761b41c8e409e86cb4cb5c4368a
calebflores <calebflores1388@gmail.com>
2024-10-23 16:57:20 -0500

    3 speed types added

---
+++ speed/levi.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Levi',
+        nen_type: 'Enhancer',
+        element: 'Lightning',
+        elm: lightning,
+        hp: 3087,
+        //PHYSICAL
+        attack: 145,
+        defense: 199,
+        critical: .65,
+        strength: 1.25,
+        //MAGIC
+        elm_attack: 165,
+        elm_defense: 169,
+        elm_efficiency: 1.7,
+        potency: .18,
+        //WEAPON
+        weapon: 247,
+        shield: 221,
+        curse_crit: .18,
+        weaponCurse: 'Attack',
+        weapon_amount: 15,
+        //SPEED / LUCK
+        accuracy: 9,
+        evasion: 12,
+        luck: 7,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 1,
+        gen_power: 0,
+        willpower: 0,
+        chakra: 0,
+        chakra_rec: 5,
+        drain: 2,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 16,
+        negative_energy: 12,
+        cursed_tech: 'Polymorphic - Increase Strength to 5',
+        curse_amount: 3.75,
+        curse_duration: 6,
+        curse_cost: 20,
+        //DOMAIN
+        dom_purity: 5,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Thunder-Spears - Increase Potency by 20%',
+        ninJutsu2cost: 11,
+        ninJutsu2Dmg: 195,
+        nin2amount: .20,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Ackerman-Curse - Increase Weapon by .10",
+        nin_cost: 13,
+        ninjutsu_pow: 185,
+        ninamount: .10,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Stun by 12%',
+        buff2: 'Decrease Magic by 15%',
+        buff_acc: 50,
+        buff2_acc: 70,
+        image: ''
+    },
+]
---
+++ speed/neferpitou.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Neferpitou',
+        nen_type: 'Specialist',
+        element: 'Dark',
+        elm: dark,
+        hp: 2976,
+        //PHYSICAL
+        attack: 255,
+        defense: 179,
+        critical: .20,
+        strength: 1.6,
+        //MAGIC
+        elm_attack: 131,
+        elm_defense: 172,
+        elm_efficiency: 1.2,
+        potency: .08,
+        //WEAPON
+        weapon: 145,
+        shield: 166,
+        curse_crit: .10,
+        weaponCurse: 'Accuracy',
+        weapon_amount: 1,
+        //SPEED / LUCK
+        accuracy: 7,
+        evasion: 11,
+        luck: 5,
+        stun: .07,
+        //GENJUTSU
+        genjutsu: 1,
+        gen_power: 0,
+        willpower: 0,
+        chakra: 0,
+        chakra_rec: 6,
+        drain: 3,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 45,
+        negative_energy: 14,
+        cursed_tech: '',
+        curse_amount: 0,
+        curse_duration: 0,
+        curse_cost: 0,
+        //DOMAIN
+        dom_purity: 2,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Terpsichora - Increase critical by 30%',
+        ninJutsu2cost: 10,
+        ninJutsu2Dmg: 125,
+        nin2amount: .30,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Doctor Blythe - Recover 400 HP",
+        nin_cost: 16,
+        ninjutsu_pow: 0,
+        ninamount: 400,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Stun by 20%',
+        buff2: 'Decrease Stun to 0',
+        buff_acc: 50,
+        buff2_acc: 50,
+        image: 'https://static.wikia.nocookie.net/hunterxhunter/images/2/20/109_-_Neferpitou_portrait.png/revision/latest?cb=20131218034405'
+    },
+]
