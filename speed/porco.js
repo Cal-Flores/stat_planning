c0265d6f3bb5fc27eaa0ccb1d838db34390e6eba
calebflores <calebflores1388@gmail.com>
2024-10-23 23:12:37 -0500

    3 speed types added

---
+++ speed/porco.js
@@ -0,0 +1,68 @@
+[
+    {
+        name: 'Jaw Titan',
+        nen_type: 'Enhancer',
+        element: 'Fighter',
+        elm: fighter,
+        hp: 2988,
+        //PHYSICAL
+        attack: 246,
+        defense: 160,
+        critical: .70,
+        strength: 1.3,
+        //MAGIC
+        elm_attack: 155,
+        elm_defense: 170,
+        elm_efficiency: 1,
+        potency: .15,
+        //WEAPON
+        weapon: 256,
+        shield: 187,
+        curse_crit: .25,
+        weaponCurse: 'Defense',
+        weapon_amount: 20,
+        //SPEED / LUCK
+        accuracy: 7,
+        evasion: 11,
+        luck: 3,
+        stun: .06,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 2,
+        chakra: 0,
+        chakra_rec: 7,
+        drain: 8,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 35,
+        negative_energy: 10,
+        cursed_tech: 'Simple-Domain - Increase Defense by 70',
+        curse_amount: 70,
+        curse_duration: 6,
+        curse_cost: 14,
+        //DOMAIN
+        dom_purity: 2,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Jaws - Increase Weapon by 5%',
+        ninJutsu2cost: 15,
+        ninJutsu2Dmg: 250,
+        nin2amount: .05,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Cystal-Claws - Increase Evasion by 1",
+        nin_cost: 15,
+        ninjutsu_pow: 160,
+        ninamount: 1,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Stun by 10%',
+        buff2: 'Decrease Weapon by 15%',
+        buff_acc: 65,
+        buff2_acc: 65,
+        image: 'https://static.wikia.nocookie.net/shingekinokyojin/images/d/da/Jaw_Titan_%28Anime%29_character_image_%28Porco_Galliard%29.png/revision/latest?cb=20201220224328'
+    },
+]
