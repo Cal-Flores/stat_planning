44c3652b5b43c168c3993b754ee60bbf75ab6c5a
calebflores <calebflores1388@gmail.com>
2024-11-06 17:22:54 -0600

    best curse domain added

---
+++ domain/gojo.js
@@ -0,0 +1,69 @@
+[
+    {
+        name: 'Gojo',
+        nen_type: 'Specialist',
+        element: 'Ghost',
+        elm: ghost,
+        hp: 3444,
+        //PHYSICAL
+        attack: 214,
+        defense: 224,
+        critical: .27,
+        strength: 1.70,
+        //MAGIC
+        elm_attack: 228,
+        elm_defense: 218,
+        elm_efficiency: 1.31,
+        potency: .32,
+        //WEAPON
+        weapon: 139,
+        shield: 146,
+        curse_crit: .15,
+        weaponCurse: 'Accuracy',
+        weapon_amount: 2,
+        //SPEED / LUCK
+        accuracy: 7,
+        evasion: 6,
+        luck: 8,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 50,
+        willpower: 4,
+        chakra: 0,
+        chakra_rec: 7,
+        drain: 5,
+        //CURSE
+        cursed_energy: 10,
+        cursed_recovery: 80,
+        negative_energy: 50,
+        cursed_tech: 'Limitless - 12 | 20 ',
+        curse_amount: 10,
+        curse_duration: 6,
+        curse_cost: 12,
+        //DOMAIN
+        dom_purity: 15,
+        dom_lethal: 100,
+        dom_duration: 10,
+        dom_cost: 30,
+        //NINJUTSU
+        ninjutsu2: 'Six-Eyes - Increase Curse Recovery',
+        ninJutsu2cost: 16,
+        ninJutsu2Dmg: 0,
+        nin2amount: 15,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Hollow-Technique: Purple",
+        nin_cost: 30,
+        ninjutsu_pow: 500,
+        ninamount: 0,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Shield by 10%',
+        buff2: 'Decrease Genjutsu by 5',
+        buff_acc: 70,
+        buff2_acc: 50,
+        image: 'https://static.wikia.nocookie.net/theunitedorganizationtoonsheroes/images/7/77/Satoru_Gojo.jpg/revision/latest/scale-to-width-down/450?cb=20220201130911'
+    },
+
+]
---
+++ domain/sukuna.js
@@ -0,0 +1,69 @@
+[
+    {
+        name: 'Sukuna',
+        nen_type: 'Transmuter',
+        element: 'Fire',
+        elm: fire,
+        hp: 3389,
+        //PHYSICAL
+        attack: 137,
+        defense: 143,
+        critical: .12,
+        strength: 1.25,
+        //MAGIC
+        elm_attack: 288,
+        elm_defense: 189,
+        elm_efficiency: 1.42,
+        potency: .25,
+        //WEAPON
+        weapon: 250,
+        shield: 185,
+        curse_crit: .12,
+        weaponCurse: 'Magic Def',
+        weapon_amount: 20,
+        //SPEED / LUCK
+        accuracy: 9,
+        evasion: 5,
+        luck: 5,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 4,
+        gen_power: 40,
+        willpower: 4,
+        chakra: 0,
+        chakra_rec: 6,
+        drain: 3,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 70,
+        negative_energy: 25,
+        cursed_tech: 'Malevolent Shrine',
+        curse_amount: 0,
+        curse_duration: 6,
+        curse_cost: 20,
+        //DOMAIN
+        dom_purity: 14,
+        dom_lethal: 220,
+        dom_duration: 5,
+        dom_cost: 50,
+        //NINJUTSU
+        ninjutsu2: 'Divine-Flame - Increase Potency by 30%',
+        ninJutsu2cost: 14,
+        ninJutsu2Dmg: 150,
+        nin2amount: .30,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Shrine - Decrease any stat by 18%",
+        nin_cost: 22,
+        ninjutsu_pow: 0,
+        ninamount: .18,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Magic Defense by 10%',
+        buff2: 'Decrease Evasion by 3',
+        buff_acc: 65,
+        buff2_acc: 50,
+        image: 'https://pbs.twimg.com/media/ErKDr0UXAAAFux9.jpg'
+    },
+
+]
