d298de901f79d47f7718454f4c898e0436305e5d
calebflores <calebflores1388@gmail.com>
2024-11-11 22:48:06 -0600

    stagglers added

---
+++ emitters/pain.js
@@ -0,0 +1,75 @@
+[
+    {
+        name: 'Pain',
+        nen_type: 'Specialist',
+        element: 'Psychic',
+        elm: psychic,
+        hp: 3247,
+        //PHYSICAL
+        attack: 124,
+        defense: 219,
+        critical: .10,
+        strength: 1.16,
+        //MAGIC
+        elm_attack: 281,
+        elm_defense: 222,
+        elm_efficiency: 1,
+        potency: .18,
+        //WEAPON
+        weapon: 199,
+        shield: 178,
+        curse_crit: .50,
+        weaponCurse: 'Recovery',
+        weapon_amount: 1,
+        //SPIRIT
+        spirit: 155,
+        spirit_def: 164,
+        spirit_crit: .15,
+        spiritCurse: 'Damage',
+        spirit_amount: 100,
+        //SPEED / LUCK
+        accuracy: 10,
+        evasion: 2,
+        luck: 2,
+        stun: .04,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 80,
+        willpower: 4,
+        chakra: 0,
+        chakra_rec: 7,
+        drain: 8,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 35,
+        negative_energy: 30,
+        cursed_tech: 'Gakido - No damage from previous Ninjutsu | Do a boost',
+        curse_amount: 0,
+        curse_duration: 1,
+        curse_cost: 15,
+        //DOMAIN
+        dom_purity: 7,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Shinra-Tensei - Decrease all 3 Attacks',
+        ninJutsu2cost: 20,
+        ninJutsu2Dmg: 180,
+        nin2amount: .12,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Chibaku-Tensei - Erase Magic Defense",
+        nin_cost: 60,
+        ninjutsu_pow: 260,
+        ninamount: 100,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Shura Canon - Deal 400 Damage (Single Use)',
+        buff2: 'Rinne Rebirth - Recover 400 HP (Single Use)',
+        buff_acc: 40,
+        buff2_acc: 40,
+        image: ''
+    },
+
+]
---
+++ enhancers/yuki.js
@@ -0,0 +1,75 @@
+[
+    {
+        name: 'Yuki',
+        nen_type: 'Conjuror',
+        element: 'Ghost',
+        elm: ghost,
+        hp: 3139,
+        //PHYSICAL
+        attack: 256,
+        defense: 173,
+        critical: .24,
+        strength: 1.24,
+        //MAGIC
+        elm_attack: 148,
+        elm_defense: 187,
+        elm_efficiency: 1.18,
+        potency: .16,
+        //WEAPON
+        weapon: 248,
+        shield: 224,
+        curse_crit: .17,
+        weaponCurse: 'Defense',
+        weapon_amount: 12,
+        //SPIRIT
+        spirit: 121,
+        spirit_def: 164,
+        spirit_crit: .15,
+        spiritCurse: 'Attack',
+        spirit_amount: 10,
+        //SPEED / LUCK
+        accuracy: 6,
+        evasion: 7,
+        luck: 3,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 15,
+        willpower: 3,
+        chakra: 0,
+        chakra_rec: 4,
+        drain: 4,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 35,
+        negative_energy: 35,
+        cursed_tech: 'Garuda - Increase Weapon to 310',
+        curse_amount: 62,
+        curse_duration: 10,
+        curse_cost: 25,
+        //DOMAIN
+        dom_purity: 7,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Star-Rage - Increase Attack by 8%',
+        ninJutsu2cost: 11,
+        ninJutsu2Dmg: 150,
+        nin2amount: .08,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Reverse-Curse-Technique - Heal Previous Damage",
+        nin_cost: 26,
+        ninjutsu_pow: 0,
+        ninamount: 0,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Spirit Defense by 10%',
+        buff2: 'Decrease Spirit by 10%',
+        buff_acc: 60,
+        buff2_acc: 65,
+        image: 'https://tiermaker.com/images/media/template_images/2024/867926/jujutsu-kaisen-characters-show-867926/zzzzz-1707450329img7500.png'
+    },
+
+]
