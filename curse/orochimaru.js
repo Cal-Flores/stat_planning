02719d43c11a94c1d3948ff7b83df9f4ee9a4ee6
calebflores <calebflores1388@gmail.com>
2024-11-13 02:06:33 -0600

    curse ckeanup added

---
+++ curse/orochimaru.js
@@ -0,0 +1,75 @@
+[
+    {
+        name: 'Orochimaru',
+        nen_type: 'Manipulator',
+        element: 'Grass',
+        elm: grass,
+        hp: 3179,
+        //PHYSICAL
+        attack: 128,
+        defense: 224,
+        critical: .12,
+        strength: 1.43,
+        //MAGIC
+        elm_attack: 142,
+        elm_defense: 166,
+        elm_efficiency: 1.40,
+        potency: .23,
+        //WEAPON
+        weapon: 218,
+        shield: 179,
+        curse_crit: .20,
+        weaponCurse: 'Accuracy',
+        weapon_amount: 1,
+        //SPIRIT
+        spirit: 259,
+        spirit_def: 175,
+        spirit_crit: .10,
+        spiritCurse: 'Weapon',
+        spirit_amount: 10,
+        //SPEED / LUCK
+        accuracy: 6,
+        evasion: 5,
+        luck: 5,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 5,
+        gen_power: 40,
+        willpower: 6,
+        chakra: 0,
+        chakra_rec: 6,
+        drain: 6,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 45,
+        negative_energy: 45,
+        cursed_tech: 'Eight-Branch-Snake-Technique - Increase Magic to 350',
+        curse_amount: 108,
+        curse_duration: 10,
+        curse_cost: 30,
+        //DOMAIN
+        dom_purity: 4,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'White-Snake - Damage a Stat by 10%',
+        ninJutsu2cost: 14,
+        ninJutsu2Dmg: 145,
+        nin2amount: 0,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Rashōmon Gates - Increase All Defenses",
+        nin_cost: 22,
+        ninjutsu_pow: 180,
+        ninamount: .13,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Evasion by 2',
+        buff2: 'Decrease Evasion by 3',
+        buff_acc: 70,
+        buff2_acc: 60,
+        image: 'https://preview.redd.it/prime-hiruzen-loses-to-orochimaru-v0-bofksvybr5gc1.jpeg?auto=webp&s=3fb6efa6db2fdc08da3a5bb702a9a50bd68e5f4c'
+    },
+
+]
---
+++ curse/sai.js
@@ -0,0 +1,69 @@
+[
+    {
+        name: 'Sai',
+        nen_type: 'Conjuror',
+        element: 'Dark',
+        elm: dark,
+        hp: 2974,
+        //PHYSICAL
+        attack: 119,
+        defense: 188,
+        critical: .12,
+        strength: 1.11,
+        //MAGIC
+        elm_attack: 128,
+        elm_defense: 147,
+        elm_efficiency: 1.20,
+        potency: .30,
+        //WEAPON
+        weapon: 192,
+        shield: 215,
+        curse_crit: .20,
+        weaponCurse: 'S-Def',
+        weapon_amount: 7,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 5,
+        luck: 7,
+        stun: .08,
+        //GENJUTSU
+        genjutsu: 5,
+        gen_power: 38,
+        willpower: 5,
+        chakra: 0,
+        chakra_rec: 6,
+        drain: 6,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 48,
+        negative_energy: 40,
+        cursed_tech: 'Super-Beast-Hawk - Increase Evasion by 8',
+        curse_amount: 8,
+        curse_duration: 6,
+        curse_cost: 16,
+        //DOMAIN
+        dom_purity: 6,
+        dom_lethal: 90,
+        dom_duration: 6,
+        dom_cost: 25,
+        //NINJUTSU
+        ninjutsu2: 'Super-Beast-Tomoe-Lion - Decrease Spirit Defense by 8%',
+        ninJutsu2cost: 16,
+        ninJutsu2Dmg: 210,
+        nin2amount: .08,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Super-Beast-Dragon - Decrease Recovery by 3",
+        nin_cost: 20,
+        ninjutsu_pow: 235,
+        ninamount: 3,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Recovery by 3',
+        buff2: 'Decrease Evasion by 2',
+        buff_acc: 50,
+        buff2_acc: 70,
+        image: 'https://www.nautiljon.com/images/perso/00/51/sai_2715.webp'
+    },
+
+]
