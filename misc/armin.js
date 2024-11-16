1f7d25d7838a1276b3f980b60c236e2658f9cc1a
calebflores <calebflores1388@gmail.com>
2024-11-16 00:46:45 -0600

    curse ha

---
+++ misc/armin.js
@@ -0,0 +1,75 @@
+[
+    {
+        name: 'Colossal Titan',
+        nen_type: 'Emission',
+        element: 'Fire',
+        elm: fire,
+        hp: 4000,
+        //PHYSICAL
+        attack: 250,
+        defense: 240,
+        critical: .80,
+        strength: 2.5,
+        //MAGIC
+        elm_attack: 220,
+        elm_defense: 125,
+        elm_efficiency: 2,
+        potency: .75,
+        //WEAPON
+        weapon: 100,
+        shield: 100,
+        curse_crit: .10,
+        weaponCurse: 'Evasion',
+        weapon_amount: 4,
+        //SPIRIT
+        spirit: 110,
+        spirit_def: 128,
+        spirit_crit: .10,
+        spiritCurse: 'Accuracy',
+        spirit_amount: 4,
+        //SPEED / LUCK
+        accuracy: 3,
+        evasion: 1,
+        luck: 3,
+        stun: .04,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 10,
+        chakra: 0,
+        chakra_rec: 8,
+        drain: 0,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 35,
+        negative_energy: 0,
+        cursed_tech: 'Steam-Emission - Cant be touched or charge Chakra',
+        curse_amount: 0,
+        curse_duration: 6,
+        curse_cost: 12,
+        //DOMAIN
+        dom_purity: 10,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Heat - Increase Curse Recovery',
+        ninJutsu2cost: 8,
+        ninJutsu2Dmg: 200,
+        nin2amount: 10,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Explosive-Transformation - Decrease Evasion and Defense",
+        nin_cost: 70,
+        ninjutsu_pow: 1000,
+        ninamount: .75,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Accuracy by 2',
+        buff2: 'Halve Magic Defense',
+        buff_acc: 25,
+        buff2_acc: 50,
+        image: 'https://static.wikia.nocookie.net/shingekinokyojin/images/c/c9/Colossal_Titan_%28Anime%29_character_image_%28Bertholdt_Hoover%29.png/revision/latest?cb=20170513151438'
+    },
+
+]
