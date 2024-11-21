184958272e93c3d7d3cc57b4da1b46366f8b6ef6
calebflores <calebflores1388@gmail.com>
2024-11-20 19:25:53 -0600

    ready for changes

---
+++ misc/sasori.js
@@ -0,0 +1,74 @@
+[
+    {
+        name: 'Sasori',
+        nen_type: 'Manipulator',
+        element: 'Sand',
+        elm: earth,
+        hp: 3142,
+        //PHYSICAL
+        attack: 140,
+        defense: 145,
+        critical: .23,
+        strength: 1.26,
+        //MAGIC
+        elm_attack: 242,
+        elm_defense: 168,
+        elm_efficiency: .22,
+        potency: 1.31,
+        //WEAPON
+        weapon: 211,
+        shield: 227,
+        curse_crit: .24,
+        weaponCurse: 'Stun',
+        weapon_amount: 0,
+        //SPIRIT
+        spirit: 122,
+        spirit_def: 210,
+        spirit_crit: .10,
+        spiritCurse: 'Magic',
+        spirit_amount: 5,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 4,
+        luck: 4,
+        stun: .04,
+        //GENJUTSU
+        genjutsu: 4,
+        gen_power: 25,
+        willpower: 7,
+        chakra: 0,
+        chakra_rec: 7,
+        drain: 0,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 48,
+        negative_energy: 10,
+        cursed_tech: 'Albatross - Increase Element Attack by 100',
+        curse_amount: 100,
+        curse_duration: 6,
+        curse_cost: 20,
+        //DOMAIN
+        dom_purity: 3,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Iron-Sand - Cast Genjutsu Increase Magic by 6%',
+        ninJutsu2cost: 12,
+        ninJutsu2Dmg: 165,
+        nin2amount: .06,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "10,000 Puppets - Increase Attack by 100%",
+        nin_cost: 50,
+        ninjutsu_pow: 0,
+        ninamount: 1,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Accuracy by 2',
+        buff2: 'Decrease Efficieny by 80%',
+        buff_acc: 70,
+        buff2_acc: 75,
+        image: 'https://64.media.tumblr.com/689405dcbfc405b38351edb13fcc46af/063a359a0a0bb5d0-bf/s1280x1920/a2e0c9711178cfe9442ba47a6732396c1d371ac1.jpg'
+    },
+]
