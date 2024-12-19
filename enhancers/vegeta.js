a5f16632e4f88fc8d1cf0864f4b95d7cf8135c3a
calebflores <calebflores1388@gmail.com>
2024-12-18 23:11:17 -0600

    stats on spread sheet

---
+++ enhancers/vegeta.js
@@ -0,0 +1,74 @@
+[
+    {
+        name: 'Vegeta',
+        nen_type: 'Emitter',
+        element: 'Holy',
+        elm: holy,
+        hp: 3192,
+        //PHYSICAL
+        attack: 295,
+        defense: 158,
+        critical: .23,
+        strength: 1.42,
+        //MAGIC
+        elm_attack: 84,
+        elm_defense: 177,
+        elm_efficiency: 1.15,
+        potency: .12,
+        //WEAPON
+        weapon: 108,
+        shield: 164,
+        curse_crit: .12,
+        weaponCurse: 'Defense',
+        weapon_amount: 13,
+        //SPIRIT
+        spirit: 211,
+        spirit_def: 235,
+        spirit_crit: .15,
+        spiritCurse: 'Health',
+        spirit_amount: 50,
+        //SPEED / LUCK
+        accuracy: 6,
+        evasion: 7,
+        luck: 2,
+        stun: .04,
+        //GENJUTSU
+        genjutsu: 4,
+        gen_power: 10,
+        willpower: 5,
+        chakra: 0,
+        chakra_rec: 6,
+        drain: 5,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 20,
+        negative_energy: 10,
+        cursed_tech: ' Increase Evasion',
+        curse_amount: 7,
+        curse_duration: 6,
+        curse_cost: 26,
+        //DOMAIN
+        dom_purity: 5,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Big-Bang - Lower Defense by 4%',
+        ninJutsu2cost: 14,
+        ninJutsu2Dmg: 200,
+        nin2amount: .04,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Final-Flash - 7x damage recovery is halved",
+        nin_cost: 0,
+        ninjutsu_pow: 0,
+        ninamount: -3,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Shield by 7%',
+        buff2: 'Decrease Critical by 50%',
+        buff_acc: 70,
+        buff2_acc: 70,
+        image: 'https://imgix.ranker.com/user_node_img/50081/1001612115/original/strength-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=355'
+    }
+]
