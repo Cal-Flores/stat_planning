f3e13044ddc9af13862837ab5e16b30185981d0d
calebflores <calebflores1388@gmail.com>
2024-12-18 22:29:21 -0600

    stats on spread sheet

---
+++ enhancers/goku.js
@@ -0,0 +1,74 @@
+[
+    {
+        name: 'Goku',
+        nen_type: 'Enhancer',
+        element: 'Holy',
+        elm: holy,
+        hp: 3332,
+        //PHYSICAL
+        attack: 310,
+        defense: 207,
+        critical: .18,
+        strength: 1.60,
+        //MAGIC
+        elm_attack: 79,
+        elm_defense: 129,
+        elm_efficiency: .11,
+        potency: 1.0,
+        //WEAPON
+        weapon: 82,
+        shield: 218,
+        curse_crit: .20,
+        weaponCurse: 'Luck',
+        weapon_amount: 2,
+        //SPIRIT
+        spirit: 180,
+        spirit_def: 162,
+        spirit_crit: .10,
+        spiritCurse: 'Recovery',
+        spirit_amount: 2,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 7,
+        luck: 2,
+        stun: .03,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 2,
+        chakra: 0,
+        chakra_rec: 4,
+        drain: 3,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 15,
+        negative_energy: 15,
+        cursed_tech: 'Kaio-Ken - Increase attack by 50',
+        curse_amount: 50,
+        curse_duration: 6,
+        curse_cost: 30,
+        //DOMAIN
+        dom_purity: 9,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Kamehameha - Lower Curse Recovery',
+        ninJutsu2cost: 16,
+        ninJutsu2Dmg: 265,
+        nin2amount: 15,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Spirit-Bomb - 10x damage recovery goes to 0",
+        nin_cost: 0,
+        ninjutsu_pow: 0,
+        ninamount: -10,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Critical by 20%',
+        buff2: 'Decrease Critical by 30%',
+        buff_acc: 80,
+        buff2_acc: 80,
+        image: 'https://avatarfiles.alphacoders.com/627/62714.jpg'
+    }
+]
---
+++ misc/kankuro.js
@@ -0,0 +1,75 @@
+[
+    {
+        name: 'Kankuro',
+        nen_type: 'Manipulator',
+        element: 'Sand',
+        elm: sand,
+        hp: 2976,
+        //PHYSICAL
+        attack: 112,
+        defense: 164,
+        critical: .10,
+        strength: 1.10,
+        //MAGIC
+        elm_attack: 107,
+        elm_defense: 156,
+        elm_efficiency: 1.32,
+        potency: .30,
+        //WEAPON
+        weapon: 227,
+        shield: 240,
+        curse_crit: .17,
+        weaponCurse: 'Stun',
+        weapon_amount: 0,
+        //SPIRIT
+        spirit: 180,
+        spirit_def: 209,
+        spirit_crit: .10,
+        spiritCurse: 'Weapon',
+        spirit_amount: 10,
+        //SPEED / LUCK
+        accuracy: 8,
+        evasion: 3,
+        luck: 7,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 3,
+        chakra: 0,
+        chakra_rec: 7,
+        drain: 2,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 40,
+        negative_energy: 30,
+        cursed_tech: 'Veil - Increase Defense by 50%',
+        curse_amount: .50,
+        curse_duration: 8,
+        curse_cost: 22,
+        //DOMAIN
+        dom_purity: 3,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Poison-Mist - Cast Genjutsu',
+        ninJutsu2cost: 10,
+        ninJutsu2Dmg: 175,
+        nin2amount: 0,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Machine-Triple-Shot - Trap Enemy and drain Recovery by 3",
+        nin_cost: 25,
+        ninjutsu_pow: 150,
+        ninamount: 3,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Trap time by 2 turns if caught',
+        buff2: 'Decrease Weapon by 10%',
+        buff_acc: 50,
+        buff2_acc: 65,
+        image: ''
+    },
+
+]
