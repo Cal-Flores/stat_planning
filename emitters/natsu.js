7e8d58ced57bd3e2ea697d53548c79a578b3b33c
calebflores <calebflores1388@gmail.com>
2024-12-30 01:55:48 -0600

    stats on spread sheet

---
+++ emitters/natsu.js
@@ -0,0 +1,75 @@
+[
+    {
+        name: 'Natsu',
+        nen_type: 'Emitter',
+        element: 'Fire',
+        elm: fire,
+        hp: 3213,
+        //PHYSICAL
+        attack: 226,
+        defense: 169,
+        critical: .17,
+        strength: 1.62,
+        //MAGIC
+        elm_attack: 244,
+        elm_defense: 223,
+        elm_efficiency: 1.45,
+        potency: .28,
+        //WEAPON
+        weapon: 101,
+        shield: 130,
+        curse_crit: .10,
+        weaponCurse: 'M.Def',
+        weapon_amount: 4,
+        //SPIRIT
+        spirit: 120,
+        spirit_def: 126,
+        spirit_crit: .10,
+        spiritCurse: 'Magic',
+        spirit_amount: 10,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 6,
+        luck: 5,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 2,
+        chakra: 0,
+        chakra_rec: 6,
+        drain: 4,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 18,
+        negative_energy: 10,
+        cursed_tech: 'Dragon-Fist - Increase Attack by 30',
+        curse_amount: 30,
+        curse_duration: 6,
+        curse_cost: 22,
+        //DOMAIN
+        dom_purity: 3,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: "Fire-Dragon's-Roar - Increase Magic by 4%",
+        ninJutsu2cost: 18,
+        ninJutsu2Dmg: 185,
+        nin2amount: .04,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Fire-Dragon's-Iron-Fist - Increase Str by 10%",
+        nin_cost: 20,
+        ninjutsu_pow: 200,
+        ninamount: .10,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Willpower by 2',
+        buff2: 'Decrease Curse by 12',
+        buff_acc: 60,
+        buff2_acc: 60,
+        image: 'https://static.wikia.nocookie.net/fairytail/images/c/ca/Natsu_X792.png/revision/latest?cb=20181111122101'
+    },
+
+]
