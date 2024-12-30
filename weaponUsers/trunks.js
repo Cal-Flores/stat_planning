9cca88b8b455c7ab6a94f1a2d6f91fc822c755c7
calebflores <calebflores1388@gmail.com>
2024-12-30 00:33:17 -0600

    stats on spread sheet

---
+++ weaponUsers/trunks.js
@@ -0,0 +1,74 @@
+[
+    {
+        name: 'Trunks',
+        nen_type: 'Transmuter',
+        element: 'Lightning',
+        elm: Lightning,
+        hp: 3056,
+        //PHYSICAL
+        attack: 148,
+        defense: 204,
+        critical: .14,
+        strength: 1.25,
+        //MAGIC
+        elm_attack: 180,
+        elm_defense: 123,
+        elm_efficiency: .40,
+        potency: 1.17,
+        //WEAPON
+        weapon: 288,
+        shield: 154,
+        curse_crit: .15,
+        weaponCurse: 'Recovery',
+        weapon_amount: 1,
+        //SPIRIT
+        spirit: 111,
+        spirit_def: 167,
+        spirit_crit: .10,
+        spiritCurse: 'Damage',
+        spirit_amount: 10,
+        //SPEED / LUCK
+        accuracy: 7,
+        evasion: 4,
+        luck: 4,
+        stun: .03,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 20,
+        willpower: 3,
+        chakra: 0,
+        chakra_rec: 4,
+        drain: 4,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 29,
+        negative_energy: 20,
+        cursed_tech: '',
+        curse_amount: 0,
+        curse_duration: 0,
+        curse_cost: 0,
+        //DOMAIN
+        dom_purity: 0,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Aero-Blast - Lower Weapon by 8%',
+        ninJutsu2cost: 15,
+        ninJutsu2Dmg: 185,
+        nin2amount: .08,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Finish-Buster - Decrease Magic Def by 50%",
+        nin_cost: 55,
+        ninjutsu_pow: 500,
+        ninamount: .50,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Luck by 3',
+        buff2: 'Decrease Critcial by 25%',
+        buff_acc: 60,
+        buff2_acc: 65,
+        image: ''
+    }
+]
